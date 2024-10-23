8545b7d3eb50531cd5887c980d830a5a0f23f46b
Omid Jafari <omid.jafari@bcm.edu>
2024-10-23 09:39:56 -0500

    Fixed GitHub actions build error

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,10 +1,20 @@
-"use client";
-
-import React from "react";
+import React, { Suspense } from "react";
 import MainPage from "@/components/MainPage";
 
+
+export async function generateStaticParams() {
+  return [
+    { slug: ['criteria'] },
+    { slug: ['statistics'] },
+    { slug: ['summary'] },
+    { slug: ['criteria', 'risk-score'] },
+    { slug: ['criteria', 'bleeding-exclusion'] },
+    { slug: ['criteria', 'trial-exclusion'] },
+  ];
+}
+
 const CatchAllPage = () => {
-  return <MainPage />;
+  return <Suspense fallback={null}><MainPage /></Suspense>;
 };
 
 export default CatchAllPage;
